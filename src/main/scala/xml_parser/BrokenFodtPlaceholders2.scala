68009e4d57706065996ff3c075732e1f74c6c6df
Bunyod <bunyodreal@gmail.com>
2018-02-05 14:53:00 +0500

    regex improvements

--- src/main/scala/xml_parser/BrokenFodtPlaceholders2.scala
+++ src/main/scala/xml_parser/BrokenFodtPlaceholders2.scala
@@ -1,11 +1,7 @@
 package xml_parser
+
 import java.nio.file.{Files, Path, Paths, StandardCopyOption}
-
-import com.artofsolving.jodconverter.openoffice.connection.SocketOpenOfficeConnection
-import com.artofsolving.jodconverter.openoffice.converter.OpenOfficeDocumentConverter
-
 import scala.collection.JavaConverters._
-
 import scala.io.Source
 
 object BrokenFodtPlaceholders2 extends App {
@@ -14,11 +10,15 @@ object BrokenFodtPlaceholders2 extends App {
 
   var s = lines
 
-  val pattern1 = "(>\\s*%\\s(<.*?>)\\s%)".r
+  val pattern0 = "(>\\s*%\\s*%\\s*)".r
+  pattern0.findAllMatchIn(s).foreach { matches =>
+    s = s.replace(matches.toString(), s">%%")
+  }
+  val pattern1 = "(>\\s*%\\s*(<.*?>)\\s*%)".r
   pattern1.findAllMatchIn(s).foreach { matches =>
     s = s.replace(matches.toString(), s">${matches.group(2)}%%")
   }
-  val pattern2 = ">*%%\\s(<.*?>){0,20}(.*?)%%\\s<".r
+  val pattern2 = ">\\s*%%\\s*(<.*?>)(.*?)%%\\s*<".r
   val pattern3 = "<.*?>".r
   pattern2.findAllMatchIn(s).foreach { matches =>
     val line = matches.toString()
@@ -27,7 +27,7 @@ object BrokenFodtPlaceholders2 extends App {
     val linePattern = pattern3.findAllMatchIn(text).toList
     val placeholder = linePattern.foldLeft(text) {
       (res, t) => res.replace(t.toString(), "")
-    }
+    }.replace(" ", "")
     if (labels != null) {
       if (matches.toString().startsWith(">"))
         s = s.replace(line, s">$labels${linePattern.mkString}%%$placeholder%%<")
@@ -35,6 +35,8 @@ object BrokenFodtPlaceholders2 extends App {
         s = s.replace(line, s"$labels${linePattern.mkString}%%$placeholder%%<")
     }
   }
+
+//  println(s)
 
   val tempFilesDir = Paths.get(".")
   fodtToPdfAndGetPaths(tempFilesDir, s, "tmpFile")
@@ -45,21 +47,21 @@ object BrokenFodtPlaceholders2 extends App {
   def fodtToPdfAndGetPaths(path: Path, fodtStr: String, tempFileName: String) = {
     val tmpFodtFile = createTempFile(path, tempFileName, ".odt")
     val tmpPdfFile = createTempFile(path, tempFileName, ".pdf")
-    var connection: SocketOpenOfficeConnection = null
+//    var connection: SocketOpenOfficeConnection = null
     try {
       Files.write(tmpFodtFile, Seq(fodtStr).asJava)
-      connection = new SocketOpenOfficeConnection(8100)
-      connection.connect()
-      val converter = new OpenOfficeDocumentConverter(connection)
-      converter.convert(tmpFodtFile.toFile, tmpPdfFile.toFile) // use tempDocFile if you need docFile
+//      connection = new SocketOpenOfficeConnection(8100)
+//      connection.connect()
+//      val converter = new OpenOfficeDocumentConverter(connection)
+//      converter.convert(tmpFodtFile.toFile, tmpPdfFile.toFile) // use tempDocFile if you need docFile
     } catch { case e: Throwable =>
       val problematicFileCopy = createTempFile(path, s"${tempFileName}_problematic", ".odt")
       Files.copy(tmpFodtFile.toAbsolutePath, problematicFileCopy, StandardCopyOption.REPLACE_EXISTING)
       println(s"Error while converting to PDF; input [$tmpFodtFile]; output [$tmpPdfFile]") // use tempDocFile if you need docFile
     } finally {
-      if (connection != null) {
-        connection.disconnect()
-      }
+//      if (connection != null) {
+//        connection.disconnect()
+//      }
     }
   }
 }
